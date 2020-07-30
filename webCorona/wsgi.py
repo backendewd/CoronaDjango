8d3bbd179d13ac188866197ba545ac6121d0b129
GaniyaMustafa <ganiyamustaga32@gmail.com>
2020-07-30 22:00:12 +0700

    ''

--- webCorona/wsgi.py
+++ webCorona/wsgi.py
@@ -1,7 +1,7 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webCorona.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webCorona.settings')
+
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
