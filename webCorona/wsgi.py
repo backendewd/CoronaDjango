da9dc9ba3bcb3bb5bb280d7cff8c31095740e63c
GaniyaMustafa <ganiyamustaga32@gmail.com>
2020-07-30 21:53:46 +0700

    ''

--- webCorona/wsgi.py
+++ webCorona/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for webCorona project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webCorona.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webCorona.settings')
-
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
